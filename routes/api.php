11c357ab7e137863f833e1aaa3750ae0c917f76e
Rzz13 <riskiassabi@gmail.com>
2026-05-22 10:18:16 +0800

    add service route

---
+++ routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\Api\ServiceController;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('services', ServiceController::class);
+Route::patch("services/{service}/change-status", [ServiceController::class, 'changeServiceStatus']);
