cad174c03c2d3cbc1191d029304d63a5fa05fa9b
Rzz13 <riskiassabi@gmail.com>
2026-05-22 16:57:25 +0800

    removed getCustomerById in controller

--- routes/api.php
+++ routes/api.php
@@ -9,7 +9,5 @@ Route::patch("services/{service}/change-status", [ServiceController::class, 'cha
 
 // Customer routes
 Route::get("customers/status", [CustomerController::class, 'getDataByStatus']);
-
 Route::apiResource('customers', CustomerController::class);
 Route::patch("customers/{customer}/change-status", [CustomerController::class, 'changeCustomerStatus']);
-Route::get("customers/{id}", [CustomerController::class, 'getDataById']);
